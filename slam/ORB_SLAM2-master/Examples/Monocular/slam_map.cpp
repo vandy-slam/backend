72433682c29302c111c59499db730b7aa29a0ccb
zappyfish <liamkelly2015@gmail.com>
2018-11-03 15:00:23 -0500

    updates

---
+++ slam/ORB_SLAM2-master/Examples/Monocular/slam_map.cpp
@@ -0,0 +1,25 @@
+//
+// Created by Liam Kelly on 11/3/18.
+//
+
+//
+// Created by Liam Kelly on 11/3/18.
+//
+#include <string>
+
+struct SLAMMap
+{
+    SLAMap(std::string map_name) {};
+    std::string test() {return "test"};
+};
+
+#include <boost/python.hpp>
+using namespace boost::python;
+
+BOOST_PYTHON_MODULE(slam_map)
+        {
+                class_<SLAMMap>("slam_map", init<std::string>())
+                        .def("test", &SLAMMap::test)
+                ;
+
+        }
---
+++ slam/ORB_SLAM2-master/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/DependInfo.cmake
@@ -0,0 +1,51 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/liamkelly/vandyhacks/slam/ORB_SLAM2-master/src/Converter.cc" "/Users/liamkelly/vandyhacks/slam/ORB_SLAM2-master/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/Converter.cc.o"
+  "/Users/liamkelly/vandyhacks/slam/ORB_SLAM2-master/src/Frame.cc" "/Users/liamkelly/vandyhacks/slam/ORB_SLAM2-master/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/Frame.cc.o"
+  "/Users/liamkelly/vandyhacks/slam/ORB_SLAM2-master/src/FrameDrawer.cc" "/Users/liamkelly/vandyhacks/slam/ORB_SLAM2-master/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/FrameDrawer.cc.o"
+  "/Users/liamkelly/vandyhacks/slam/ORB_SLAM2-master/src/Initializer.cc" "/Users/liamkelly/vandyhacks/slam/ORB_SLAM2-master/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/Initializer.cc.o"
+  "/Users/liamkelly/vandyhacks/slam/ORB_SLAM2-master/src/KeyFrame.cc" "/Users/liamkelly/vandyhacks/slam/ORB_SLAM2-master/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/KeyFrame.cc.o"
+  "/Users/liamkelly/vandyhacks/slam/ORB_SLAM2-master/src/KeyFrameDatabase.cc" "/Users/liamkelly/vandyhacks/slam/ORB_SLAM2-master/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/KeyFrameDatabase.cc.o"
+  "/Users/liamkelly/vandyhacks/slam/ORB_SLAM2-master/src/LocalMapping.cc" "/Users/liamkelly/vandyhacks/slam/ORB_SLAM2-master/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/LocalMapping.cc.o"
+  "/Users/liamkelly/vandyhacks/slam/ORB_SLAM2-master/src/LoopClosing.cc" "/Users/liamkelly/vandyhacks/slam/ORB_SLAM2-master/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/LoopClosing.cc.o"
+  "/Users/liamkelly/vandyhacks/slam/ORB_SLAM2-master/src/Map.cc" "/Users/liamkelly/vandyhacks/slam/ORB_SLAM2-master/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/Map.cc.o"
+  "/Users/liamkelly/vandyhacks/slam/ORB_SLAM2-master/src/MapDrawer.cc" "/Users/liamkelly/vandyhacks/slam/ORB_SLAM2-master/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/MapDrawer.cc.o"
+  "/Users/liamkelly/vandyhacks/slam/ORB_SLAM2-master/src/MapPoint.cc" "/Users/liamkelly/vandyhacks/slam/ORB_SLAM2-master/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/MapPoint.cc.o"
+  "/Users/liamkelly/vandyhacks/slam/ORB_SLAM2-master/src/ORBextractor.cc" "/Users/liamkelly/vandyhacks/slam/ORB_SLAM2-master/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/ORBextractor.cc.o"
+  "/Users/liamkelly/vandyhacks/slam/ORB_SLAM2-master/src/ORBmatcher.cc" "/Users/liamkelly/vandyhacks/slam/ORB_SLAM2-master/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/ORBmatcher.cc.o"
+  "/Users/liamkelly/vandyhacks/slam/ORB_SLAM2-master/src/Optimizer.cc" "/Users/liamkelly/vandyhacks/slam/ORB_SLAM2-master/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/Optimizer.cc.o"
+  "/Users/liamkelly/vandyhacks/slam/ORB_SLAM2-master/src/PnPsolver.cc" "/Users/liamkelly/vandyhacks/slam/ORB_SLAM2-master/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/PnPsolver.cc.o"
+  "/Users/liamkelly/vandyhacks/slam/ORB_SLAM2-master/src/Sim3Solver.cc" "/Users/liamkelly/vandyhacks/slam/ORB_SLAM2-master/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/Sim3Solver.cc.o"
+  "/Users/liamkelly/vandyhacks/slam/ORB_SLAM2-master/src/System.cc" "/Users/liamkelly/vandyhacks/slam/ORB_SLAM2-master/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/System.cc.o"
+  "/Users/liamkelly/vandyhacks/slam/ORB_SLAM2-master/src/Tracking.cc" "/Users/liamkelly/vandyhacks/slam/ORB_SLAM2-master/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/Tracking.cc.o"
+  "/Users/liamkelly/vandyhacks/slam/ORB_SLAM2-master/src/Viewer.cc" "/Users/liamkelly/vandyhacks/slam/ORB_SLAM2-master/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/Viewer.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "COMPILEDWITHC11"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../"
+  "../include"
+  "/usr/local/include/eigen3"
+  "/Users/liamkelly/slam/Pangolin/include"
+  "/Users/liamkelly/slam/Pangolin/build/src/include"
+  "/System/Library/Frameworks/OpenGL.framework"
+  "/usr/local/include"
+  "/usr/local/include/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")

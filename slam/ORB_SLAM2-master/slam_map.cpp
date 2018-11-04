2d226479aeb5daaa3bff477a86f7d480934aa724
zappyfish <liamkelly2015@gmail.com>
2018-11-04 02:45:18 -0600

    pls compile

--- slam/ORB_SLAM2-master/slam_map.cpp
+++ slam/ORB_SLAM2-master/slam_map.cpp
@@ -220,7 +220,7 @@ BOOST_PYTHON_MODULE(slam_map)
 
                 class_<PointMap>("point_map", init<>())
                         .def("num_pts", &PointMap::num_pts)
-                        .def("pt_at" &PointMap::pt_at)
+                        .def("pt_at", &PointMap::pt_at)
                 ;
 
         }

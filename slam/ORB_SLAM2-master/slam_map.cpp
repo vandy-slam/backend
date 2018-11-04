e8f8d38a6569c431138a24341acbd7f13ca5a070
zappyfish <liamkelly2015@gmail.com>
2018-11-04 01:05:49 -0600

    compile

--- slam/ORB_SLAM2-master/slam_map.cpp
+++ slam/ORB_SLAM2-master/slam_map.cpp
@@ -18,21 +18,15 @@ using namespace std;
 class SLAMMap
 {
 
-public:
-
-    SLAMMap(string map_name, string arg1, string arg2, string arg3): SLAM(arg1, arg2, ORB_SLAM2::System::MONOCULAR, false)
-     {
-        
+    SLAMMap(string map_name) {
+        init(map_name, this->SLAM);
     }
-
     SLAMMap() {
-        string arg1, arg2, arg3;
-        get_args(map_name, arg1, arg2, arg3);
-        SLAMMap(TEST, arg1, arg2, arg3);
+        SLAMMap(TEST);
     }
 
     void check() {
-        if (SLAM == nullptr) {
+        if (this->SLAM == nullptr) {
             cout << "is null\n";
         } else {
             cout << "is NOT null \n";
@@ -47,6 +41,76 @@ public:
         return pts;
     }
 
+    void init(std::string &map_name, ORB_SLAM2::System* &SLAM) {
+        string arg1, arg2, arg3;
+
+        get_args(map_name, arg1, arg2, arg3);
+
+        vector<string> vstrImageFilenames;
+        vector<double> vTimestamps;
+        string strFile = arg3+"/rgb.txt";
+        LoadImages(strFile, vstrImageFilenames, vTimestamps);
+
+        SLAM = new ORB_SLAM2::System(arg1, arg2, ORB_SLAM2::System::MONOCULAR, false);
+
+        int nImages = vstrImageFilenames.size();
+
+        vector<float> vTimesTrack;
+        vTimesTrack.resize(nImages);
+
+        cout << endl << "-------" << endl;
+        cout << "Start processing sequence ..." << endl;
+        cout << "Images in the sequence: " << nImages << endl << endl;
+
+        // Main loop
+        cv::Mat im;
+        for(int ni=0; ni<nImages; ni++)
+        {
+            // Read image from file
+            im = cv::imread(arg3+"/"+vstrImageFilenames[ni],CV_LOAD_IMAGE_UNCHANGED);
+            double tframe = vTimestamps[ni];
+
+            if(im.empty())
+            {
+                cerr << endl << "Failed to load image at: "
+                     << string(arg3) << "/" << vstrImageFilenames[ni] << endl;
+                return;
+            }
+
+#ifdef COMPILEDWITHC11
+            std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
+#else
+            std::chrono::monotonic_clock::time_point t1 = std::chrono::monotonic_clock::now();
+#endif
+
+            // Pass the image to the SLAM system
+            SLAM->TrackMonocular(im,tframe);
+
+#ifdef COMPILEDWITHC11
+            std::chrono::steady_clock::time_point t2 = std::chrono::steady_clock::now();
+#else
+            std::chrono::monotonic_clock::time_point t2 = std::chrono::monotonic_clock::now();
+#endif
+
+            double ttrack= std::chrono::duration_cast<std::chrono::duration<double> >(t2 - t1).count();
+
+            vTimesTrack[ni]=ttrack;
+
+            cout << "step took: " << ttrack << " s\n";
+
+            // Wait to load the next frame
+            double T=0;
+            if(ni<nImages-1)
+                T = vTimestamps[ni+1]-tframe;
+            else if(ni>0)
+                T = tframe-vTimestamps[ni-1];
+
+            if(ttrack<T)
+                usleep((T-ttrack)*1e6);
+        }
+        cout << "done initing\n";
+    }
+
     void get_args(string &map_name, string &arg1, string &arg2, string &arg3) {
         if (map_name == TEST) {
             arg1 = "Vocabulary/ORBvoc.txt";
@@ -59,7 +123,7 @@ public:
 
     string test() {return TEST;}
 
-    ORB_SLAM2::System SLAM;
+    std::unique_ptr<ORB_SLAM2::System>* SLAM;
 
     void LoadImages(const string &strFile, vector<string> &vstrImageFilenames, vector<double> &vTimestamps)
     {
@@ -97,7 +161,7 @@ using namespace boost::python;
 
 BOOST_PYTHON_MODULE(slam_map)
         {
-                class_<SLAMMap>("slam_map", init<string, string, string, string>())
+                class_<SLAMMap>("slam_map", init<string>())
                         .def(init<>())
                         .def("test", &SLAMMap::test)
                         .def("get_pts", &SLAMMap::get_points)

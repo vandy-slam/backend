2f2fe6e162f342acc39b557d1aad0427c296007a
zappyfish <liamkelly2015@gmail.com>
2018-11-04 01:38:20 -0600

    path

--- slam/ORB_SLAM2-master/slam_map.cpp
+++ slam/ORB_SLAM2-master/slam_map.cpp
@@ -23,7 +23,7 @@ struct SLAMMap
     }
 
     vector<double> localize_with_image(std::string img_path) {
-        cv::Mat im = cv::imread(img_path), CV_LOAD_IMAGE_UNCHANGED);
+        cv::Mat im = cv::imread(img_path, CV_LOAD_IMAGE_UNCHANGED);
         vector<double> localization;
 
 

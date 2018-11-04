180160c72fafec83b3b5bf0676a033df535b127b
zappyfish <liamkelly2015@gmail.com>
2018-11-04 02:24:36 -0600

    fdjskl

--- slam/ORB_SLAM2-master/slam_map.cpp
+++ slam/ORB_SLAM2-master/slam_map.cpp
@@ -128,6 +128,7 @@ struct SLAMMap
     vector<vector<double>> get_points() {
         vector<vector<double>> pts;
         vector<ORB_SLAM2::MapPoint*> map_pts = SLAM->GetTrackedMapPoints();
+        cout << map_pts->size();
         for (auto const &map_pt : map_pts) {
             vector<double> pt;
             cout << map_pt->GetWorldPos();
